5dfa4b559ea58f4cdcc14cd490b538bb571085c1
setkyar <setkyar16@gmail.com>
2023-09-30 16:05:07 +0800

    translate range

--- examples/range/range.go
+++ examples/range/range.go
@@ -1,15 +1,15 @@
-// _range_ iterates over elements in a variety of data
-// structures. Let's see how to use `range` with some
-// of the data structures we've already learned.
+//  _range_ က data structures တော်တော်များများရဲ့ elements တွေကို
+// iterate လုပ်တဲ့နေရာမှာသုံးတာပါ။ `range` ကိုကျနော်တို့လေ့လာထားတဲ့
+// data structures တွေမှာဘယ်လိုသုံးမလဲဆိုတာာအောက်မှာတချက်ကြည့်လိုက်ရအောင်။
 
 package main
 
 import "fmt"
 
 func main() {
+	// ဒီမှာကျနော်တို့ slice ထဲမှာရှိတဲ့နံပါတ်တွေကို `range` ကို သုံးပီး ပေါင်းပြထားပါတယ်။
+	// Array တွေလဲအဲ့လိုဘဲ အသုံးပြုပြီးပေါင်းနိုင်ပါတယ်။
 
-	// Here we use `range` to sum the numbers in a slice.
-	// Arrays work like this too.
 	nums := []int{2, 3, 4}
 	sum := 0
 	for _, num := range nums {
@@ -17,33 +17,29 @@ func main() {
 	}
 	fmt.Println("sum:", sum)
 
-	// `range` on arrays and slices provides both the
-	// index and value for each entry. Above we didn't
-	// need the index, so we ignored it with the
-	// blank identifier `_`. Sometimes we actually want
-	// the indexes though.
+	// arrays နှင့် slices တွေကို `range` ဖြင့်အသုံးပြုရင် index နှင့် value နှစ်ခုလုံးပါပါတယ်။
+	// အပေါ်ကနမူနာမှာဆိုရင် ကျနော်တို့ index မလိုတဲ့အတွက် `_` ကိုသုံးပီးတော့ ignored လုပ်ခဲ့ပါတယ်။
+	// တခါတလေကျရင်တော့ indexs တွေကိုအသုံးပြုဖို့လိုအပ်ပါတယ်။
 	for i, num := range nums {
 		if num == 3 {
 			fmt.Println("index:", i)
 		}
 	}
 
-	// `range` on map iterates over key/value pairs.
+	// `range` ကို map မှာအသုံးပြုပီးတော့ key/value pairs ကို iterate လုပ်ပြတာပါ။
 	kvs := map[string]string{"a": "apple", "b": "banana"}
 	for k, v := range kvs {
 		fmt.Printf("%s -> %s\n", k, v)
 	}
 
-	// `range` can also iterate over just the keys of a map.
+	// `range` ကိုသုံးပီးတော့ map ထဲမှာရှိတဲ့ keys တွေကိုဘဲ iterate လုပ်နိုင်ပါတယ်။
 	for k := range kvs {
 		fmt.Println("key:", k)
 	}
 
-	// `range` on strings iterates over Unicode code
-	// points. The first value is the starting byte index
-	// of the `rune` and the second the `rune` itself.
-	// See [Strings and Runes](strings-and-runes) for more
-	// details.
+	// `range` ကိုသုံးပီးတော့ string ထဲမှာရှိတဲ့ unicode code points တွေကို iterate လုပ်နိုင်ပါတယ်။
+	// ပထမ value ကတော့ character `rune` ရဲ့ starting byte index ပါပဲ။ ဒါတိယ value ကတော့ character `rune` ပါ။
+	// အသေးစိတ်ကို [Strings and Runes](strings-and-runes)  မှာကြည့်နိုင်ပါတယ်။
 	for i, c := range "go" {
 		fmt.Println(i, c)
 	}
