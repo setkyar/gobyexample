083b181ceb01ddcf6d34eb26f669df3bdd71969b
setkyar <setkyar16@gmail.com>
2023-10-02 21:36:33 +0800

    Translate recursion

--- examples/recursion/recursion.go
+++ examples/recursion/recursion.go
@@ -1,13 +1,11 @@
-// Go supports
-// <a href="https://en.wikipedia.org/wiki/Recursion_(computer_science)"><em>recursive functions</em></a>.
-// Here's a classic example.
+// Go က <a href="https://en.wikipedia.org/wiki/Recursion_(computer_science)"><em>recursive functions</em></a> ကို support လုပ်ပါတယ်။
+// Recursive function ကိုနမူနာပြနေကြ factorial နမူနာကိုတချက်ကြည့်လိုက်ကြရအောင်။
 
 package main
 
 import "fmt"
 
-// This `fact` function calls itself until it reaches the
-// base case of `fact(0)`.
+// ဒီ `fact` function က base case `fact(0)` ကို မရောက်မခြင်းသူ့ကိုယ်သူ ပြန်ခေါ်နေပါလိမ့်မယ်။
 func fact(n int) int {
 	if n == 0 {
 		return 1
@@ -18,9 +16,8 @@ func fact(n int) int {
 func main() {
 	fmt.Println(fact(7))
 
-	// Closures can also be recursive, but this requires the
-	// closure to be declared with a typed `var` explicitly
-	// before it's defined.
+	// Closures လဲ recursive ဖြစ်လို့ရတယ်၊ ဒါပေမယ့် closure function ကို defined မလုပ်ခင်
+	// type ကို `var` ဆိုပီးသီးသန့်ကြေငြာပေးရမယ်။
 	var fib func(n int) int
 
 	fib = func(n int) int {
@@ -28,8 +25,8 @@ func main() {
 			return n
 		}
 
-		// Since `fib` was previously declared in `main`, Go
-		// knows which function to call with `fib` here.
+		//  `fib` ကိုအထက်မှာကြေငြာပီးသားဖြစ်တဲ့အတွက် Go က `fib` ကိုခေါ်တဲ့အချိန်မှာ
+		//  ဘယ် funtion ကိုခေါ်ရမလဲဆိုတာကိုသိတယ်။
 		return fib(n-1) + fib(n-2)
 	}
 
