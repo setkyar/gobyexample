c970d62aee6b1a592cc199689b41877c3876bb3a
setkyar <setkyar16@gmail.com>
2024-07-01 20:52:18 +0800

    Translate pointers.go comments to Burmese

--- examples/pointers/pointers.go
+++ examples/pointers/pointers.go
@@ -1,26 +1,26 @@
-// Go supports <em><a href="https://en.wikipedia.org/wiki/Pointer_(computer_programming)">pointers</a></em>,
-// allowing you to pass references to values and records
-// within your program.
+// Go က pointers တွေကို support လုပ်ပါတယ်။
+// Go မှာ pointer ဆိုတာက variable တစ်ခုရဲ့ memory address ကို သိမ်းထားတဲ့ variable လို့ နားလည်နိုင်ပါတယ်။
+// Pointer ကိုသုံးပြီးတော့ variable ကို တိုက်ရိုက်တန်ဖိုးမပြင်ဘဲ သွယ်ဝိုက်ပြီး access လုပ်နိုင်ပါတယ်။
+// Pointer တွေက variable တစ်ခုကို copy မလုပ်ဘဲ reference ကို pass လုပ်ချင်တဲ့အခါမှာ အသုံးဝင်ပါတယ်။
 
 package main
 
 import "fmt"
 
-// We'll show how pointers work in contrast to values with
-// 2 functions: `zeroval` and `zeroptr`. `zeroval` has an
-// `int` parameter, so arguments will be passed to it by
-// value. `zeroval` will get a copy of `ival` distinct
-// from the one in the calling function.
+// pointer နဲ့ value သုံးတာ ဘာကွာခြားလဲဆိုတာကို ပြဖို့အတွက် zeroval ဆိုတဲ့ function နဲ့
+// zeroptr ဆိုတဲ့ function တွေကို သုံးပြီး ရှင်းပြပါမယ်။ zeroval function က
+// `int` parameter ကို လက်ခံပါတယ်။ ဒီမှာ ကျွန်တော်တို့က parameter မှာ value ထည့်ပေးလိုက်ပါတယ်။
+// zeroval function က ival ရဲ့ copy ကို ရရှိပါတယ်။ ဒီ copy က သူ့ကိုခေါ်တဲ့
+// function ထဲက ကောင်နဲ့ မတူဘူးဆိုတာကို သတိထားပါ။
 func zeroval(ival int) {
 	ival = 0
 }
 
-// `zeroptr` in contrast has an `*int` parameter, meaning
-// that it takes an `int` pointer. The `*iptr` code in the
-// function body then _dereferences_ the pointer from its
-// memory address to the current value at that address.
-// Assigning a value to a dereferenced pointer changes the
-// value at the referenced address.
+// zeroptr function ကတော့ `*int` parameter ရှိပါတယ်။ ဆိုလိုတာက
+// သူက `int` pointer ကို လက်ခံတယ်လို့ ဆိုလိုပါတယ်။ function ထဲမှာရှိတဲ့ `*iptr` code က
+// pointer ကို သူ့ရဲ့ memory address ကနေ လက်ရှိတန်ဖိုးဆီကို *dereference* လုပ်ပါတယ်။
+// Dereference လုပ်ထားတဲ့ pointer ကို တန်ဖိုးသတ်မှတ်ပေးခြင်းဟာ
+// ညွှန်းထားတဲ့ address မှာရှိတဲ့ တန်ဖိုးကို ပြောင်းလဲစေပါတယ်။
 func zeroptr(iptr *int) {
 	*iptr = 0
 }
@@ -32,11 +32,11 @@ func main() {
 	zeroval(i)
 	fmt.Println("zeroval:", i)
 
-	// The `&i` syntax gives the memory address of `i`,
-	// i.e. a pointer to `i`.
+	// `&i` syntax က `i` ရဲ့ memory address ကိုပေးပါတယ်။
+	// တနည်းအားဖြင့် `i` ရဲ့ pointer ပါ။
 	zeroptr(&i)
 	fmt.Println("zeroptr:", i)
 
-	// Pointers can be printed too.
+	// Pointer တွေကို Print ထုတ်ကြည့်လို့ရပါတယ်။
 	fmt.Println("pointer:", &i)
 }
--- examples/pointers/pointers.sh
+++ examples/pointers/pointers.sh
@@ -1,6 +1,9 @@
-# `zeroval` doesn't change the `i` in `main`, but
-# `zeroptr` does because it has a reference to
-# the memory address for that variable.
+# `zeroval` က `main` ထဲက `i` 
+# ကို မပြောင်းလဲစေပါဘူး၊ ဒါပေမယ့်
+# `zeroptr` ကတော့ ပြောင်းလဲစေပါတယ်။ 
+# ဘာကြောင့်လဲဆိုတော့ သူက
+# အဲဒီ variable ရဲ့ memory address 
+# ကို reference လုပ်ထားလို့ပါ။
 $ go run pointers.go
 initial: 1
 zeroval: 1
