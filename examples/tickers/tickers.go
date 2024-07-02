4580b76b457994dbb6af043ba2a2f5900e103fb9
setkyar <setkyar16@gmail.com>
2024-07-03 00:03:11 +0800

    Translate comments in tickers.go to Burmese

--- examples/tickers/tickers.go
+++ examples/tickers/tickers.go
@@ -1,8 +1,6 @@
-// [Timers](timers) are for when you want to do
-// something once in the future - _tickers_ are for when
-// you want to do something repeatedly at regular
-// intervals. Here's an example of a ticker that ticks
-// periodically until we stop it.
+// [Timer](timers) တွေက အနာဂတ်မှာ တစ်ကြိမ်တည်း တစ်ခုခုလုပ်ချင်တဲ့အခါ သုံးပါတယ် -
+// _ticker_ တွေကတော့ ပုံမှန် တစ်ခုခုကို ထပ်ခါထပ်ခါလုပ်ချင်တဲ့အချိန်မှာ သုံးပါတယ်။
+// ဒီမှာ ကျွန်တော်တို့ မရပ်ခိုင်းမချင်း ပုံမှန် tick လုပ်နေမယ့် ticker တစ်ခုရဲ့ ဥပမာကို ပြထားပါတယ်။
 
 package main
 
@@ -13,10 +11,9 @@ import (
 
 func main() {
 
-	// Tickers use a similar mechanism to timers: a
-	// channel that is sent values. Here we'll use the
-	// `select` builtin on the channel to await the
-	// values as they arrive every 500ms.
+	// Ticker တွေက timer တွေနဲ့ ဆင်တူတဲ့ နည်းလမ်းကို သုံးပါတယ်:
+	// တန်ဖိုးတွေ ပို့ပေးတဲ့ channel တစ်ခုပါ။ ဒီမှာတော့ channel ပေါ်မှာ
+	// `select` builtin ကိုသုံးပြီး 500ms တိုင်း ရောက်လာမယ့် တန်ဖိုးတွေကို စောင့်ပါမယ်။
 	ticker := time.NewTicker(500 * time.Millisecond)
 	done := make(chan bool)
 
@@ -31,9 +28,9 @@ func main() {
 		}
 	}()
 
-	// Tickers can be stopped like timers. Once a ticker
-	// is stopped it won't receive any more values on its
-	// channel. We'll stop ours after 1600ms.
+	// Ticker တွေကို timer တွေလိုပဲ ရပ်တန့်လို့ရပါတယ်။ Ticker တစ်ခုကို
+	// ရပ်တန့်လိုက်ပြီဆိုရင် သူ့ရဲ့ channel ပေါ်မှာ နောက်ထပ်တန်ဖိုးတွေ လက်ခံတော့မှာ မဟုတ်ပါဘူး။
+	// ကျွန်တော်တို့ရဲ့ ticker ကို 1600ms အကြာမှာ ရပ်တန့်ပါမယ်။
 	time.Sleep(1600 * time.Millisecond)
 	ticker.Stop()
 	done <- true
--- examples/tickers/tickers.sh
+++ examples/tickers/tickers.sh
@@ -1,5 +1,6 @@
-# When we run this program the ticker should tick 3 times
-# before we stop it.
+# ဒီပရိုဂရမ်ကို run တဲ့အခါ ticker က 
+# ကျွန်တော်တို့ မရပ်တန့်ခင် 
+# 3 ကြိမ် tick လုပ်ပါလိမ့်မယ်။
 $ go run tickers.go
 Tick at 2012-09-23 11:29:56.487625 -0700 PDT
 Tick at 2012-09-23 11:29:56.988063 -0700 PDT
